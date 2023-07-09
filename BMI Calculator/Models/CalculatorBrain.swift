92f00d196a96f98a9b80ad335cf126681b9b34ac
Tatjana Urvan <tatjana.urvan@nl.abnamro.com>
2023-07-09 14:15:33 +0200

    Push all changes to repo

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -21,17 +21,23 @@ struct CalculatorBrain {
 	
 	mutating func calculateBMI(height: Float, weight: Float) {
 		let bmiValue = weight / (height * height)
-		bmi = BMI(value: bmiValue, advice: <#T##String#>, color: <#T##UIColor#>)
 		
-		func getAdvice() {
+		
 			if bmiValue < 18.5{
-				print("Underweight")
+				bmi = BMI(value: bmiValue, advice: "Need more calories", color: .blue)
 			}else if bmiValue < 24.9 {
-				print("Normal")
+				bmi = BMI(value: bmiValue, advice: "Fit", color: .green)
 			}else {
-				print("OverWeight")
+				bmi = BMI(value: bmiValue, advice: "Too much pies", color: .red)
 		}
-		}
+	}
+	
+	func getAdvice() -> String {
+		return bmi!.advice
+	}
+	
+	func getColor() -> UIColor {
+		return bmi!.color
 	}
 	
 }
